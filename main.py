9b6b999835add49adde542669ae6b0ff0fc8cb24
NikaEG <ally.via@gmail.com>
2020-07-24 14:09:44 +0000

    YES = True

--- main.py
+++ main.py
@@ -4,21 +4,7 @@ import time
 
 words = []
 
-with open("words.txt", "r") as f:
-	for line in f.readlines():
-		words.append(line.strip())
-
-secret_word = random.choice(words)
-
-lettersnum = len(secret_word)
-
-#print('\033[91m'+" o"+'\033[0m'+" \n/|\ \n/ \ ")
-#print('\033[91m'+" o \n/"+'\033[0m'+"|\ \n/ \ ")
-#print('\033[91m'+" o \n/|"+'\033[0m'+"\ \n/ \ ")
-#print('\033[91m'+" o \n/|\ "+'\033[0m'+"\n/ \ ")
-#print('\033[91m'+" o \n/|\ \n/"+'\033[0m'+" \ ")
-#print('\033[91m'+" o \n/|\ \n/ \ ")
-
+man = 0
 print('\033[93m'+"Welcome to hangman! \n")
 time.sleep(2)
 print("The game is simple. Guess a letter; if that letter is in the word, you get a point! If it is not in the word, a body part is added to the hangman.")
@@ -36,25 +22,54 @@ if begin == "Y":
   print('\033[0m'+" o \n/|\ \n/ \ ")
   start = True
 if start == True:
-  print('\033[93m'+"The word has "+str(lettersnum)+" letters! \n")
-   #while keep_going == True:
-  correct_guessed = ""
-  all_guessed = ""
-  print(secret_word)
-  while set(correct_guessed) != set(secret_word):
-    if all_guessed:
-      print("Letters guessed: "+", ".join(sorted(all_guessed)))
-    guess = input("Guess: ")
-    all_guessed += guess
-    if guess in secret_word:
-      print("That letter IS in the word!")
-      correct_guessed += guess
-    else:
-      print("That letter is NOT in word!")
-      
-        #loop here of hangmen prints
+  keep_going == True:
+  while keep_going == True:
+    YES = True
+    secret_word = random.choice(words)
+    lettersnum = len(secret_word)
+    with open("words.txt", "r") as f:
+	    for line in f.readlines():
+		    words.append(line.strip())
+    print('\033[93m'+"The word has "+str(lettersnum)+" letters! \n")
+    correct_guessed = ""
+    all_guessed = ""
+    while set(correct_guessed) != set(secret_word):
+      if all_guessed:
+        print('\033[91m'+"All letters guessed: "+", ".join(sorted(all_guessed)))
+        print('\033[91m'+"Correct letters guessed: "+", ".join(sorted(correct_guessed)))
+      guess = input('\033[0m'+"Guess: ").lower()
+      all_guessed += guess
+      if guess in secret_word:
+        print('\033[93m'+"That letter IS in the word!")
+        correct_guessed += guess
+      else:
+        man += 1
+        print('\033[93m'+"That letter is NOT in word!")
 
-print("You guessed the word!")
+        if man == 1:
+          print('\033[91m'+" o"+'\033[0m'+" \n/|\ \n/ \ ")
+        elif man == 2:
+          print('\033[91m'+" o \n/"+'\033[0m'+"|\ \n/ \ ")
+        elif man == 3:
+          print('\033[91m'+" o \n/|"+'\033[0m'+"\ \n/ \ ")
+        elif man == 4:
+          print('\033[91m'+" o \n/|\ "+'\033[0m'+"\n/ \ ")
+        elif man == 5:
+          print('\033[91m'+" o \n/|\ \n/"+'\033[0m'+" \ ")
+        elif man == 6:
+          print('\033[91m'+" o \n/|\ \n/ \ ")
+          keep_going = False
+          print(\'033[96m'+"You loose!")
+          time.sleep(3)
+          again1 = input('\033[91m'+"Would you like to play again? Y/N? \n")
+          if again1 == "Y":
+            keep_going = True
+          elif again1 == "N":
+            keep_going = False
+            YES = False
+            print("Okay. Bye!")
 
 
-print(secret_word)
+while YES == True: #change to something else that activates when player guesses all the letters
+  print("\nYou guessed the word!")
+  print('\033[96m'+secret_word)
